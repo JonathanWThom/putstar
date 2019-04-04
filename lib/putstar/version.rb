33cf79cd8c4d4bcf265eb2ccf8378eb4a6699501
Jonathan Thom <jonathan.thom1990@gmail.com>
2019-04-03 21:41:08 -0700

    Readme fix and formatting

--- lib/putstar/version.rb
+++ lib/putstar/version.rb
@@ -1,3 +1,3 @@
 module Putstar
-  VERSION = "0.1.0"
+  VERSION = "0.1.1"
 end
--- spec/putstar_spec.rb
+++ spec/putstar_spec.rb
@@ -4,17 +4,17 @@ RSpec.describe Putstar do
   end
 
 
-   it "prints a line of asterisks above and below the object" do
-      output = File.read("./spec/fixtures/no_divider.txt")
-      expect do
-        putstar "hey" 
-      end.to output(output).to_stdout
-   end
+  it "prints a line of asterisks above and below the object" do
+    output = File.read("./spec/fixtures/no_divider.txt")
+    expect do
+      putstar "hey"
+    end.to output(output).to_stdout
+  end
 
-   it "prints a line of a custom divider above and below the object" do
-      output = File.read("./spec/fixtures/divider.txt")
-      expect do
-        putstar "hey", "$" 
-      end.to output(output).to_stdout
-   end
+  it "prints a line of a custom divider above and below the object" do
+    output = File.read("./spec/fixtures/divider.txt")
+    expect do
+      putstar "hey", "$"
+    end.to output(output).to_stdout
+  end
 end
